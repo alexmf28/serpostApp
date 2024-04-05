34a37955aa1057dc00453d570fb88daac5c68725
alexismf28 <alexismf2802@gmail.com>
2024-04-05 12:23:31 -0500

    ImplementChangesAboutMaps

--- src/presentation/providers/PermissionsChecker.tsx
+++ src/presentation/providers/PermissionsChecker.tsx
@@ -1,45 +1,53 @@
-import React, { PropsWithChildren, useEffect } from 'react'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
 import { AppState } from 'react-native'
 import { usePermissionStore } from '../store/permissions/usePermissionStore'
 import { CommonActions, useNavigation, NavigationProp } from '@react-navigation/native';
-import  { RootStackParams }  from '../navigation/StackNavigator';
+import { RootStackParams } from '../navigation/StackNavigator';
 
-export const PermissionsChecker = ({children}: PropsWithChildren) => {
+export const PermissionsChecker = ({ children }: PropsWithChildren) => {
     const { locationStatus, checkLocationPermission } = usePermissionStore();
     const navigation = useNavigation<NavigationProp<RootStackParams>>();
-
+    const [initializing, setInitializing] = useState(true);
 
     useEffect(() => {
-        if( locationStatus === 'granted' ){
-            navigation.reset({
-                routes: [{name: 'HomeMenu'}],
-            })
-        }
-        else if (locationStatus !== 'undetermined') {
-            navigation.reset({
-                routes: [{name: 'PermissionsScreen'}],
-            })
+        if (initializing) {
+            console.log('incializando')
+            console.log(initializing)         
+            setInitializing(false);
+            }
+        else {          
+            if (locationStatus === 'granted') {
+                console.log('redirige a map')
+                navigation.reset({
+                    routes: [{ name: 'MapScreen' }],
+                })
+            }
+            else if (locationStatus !== 'undetermined') {
+                navigation.reset({
+                    routes: [{ name: 'PermissionsScreen' }],
+                })
+            }
         }
     }, [locationStatus])
 
     //Para verificarlo cuando el componente se monta
-    useEffect(()=> {
+    useEffect(() => {
         checkLocationPermission();
     }, [])
 
     useEffect(() => {
         const subscription = AppState.addEventListener('change', (nextAppState) => {
-            if(nextAppState === 'active'){
+            if (nextAppState === 'active') {
                 checkLocationPermission()
-            }            
+            }
         });
 
-        return() => {
+        return () => {
             subscription.remove();
         }
     }, [])
 
     return (
-        <>{ children }</>
+        <>{children}</>
     )
 }
